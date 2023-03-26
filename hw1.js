89a8dc0e9c4f2b178472e723a81f3d20fd260ec0
«Marina» <marina_obruch@mail.ru>
2023-03-26 19:28:29 +0500

    add hw1

---
+++ hw1.js
@@ -0,0 +1,73 @@
+// Задание 1
+// let a1 = 10;
+// alert(a1);
+// a1 = 20;
+// alert(a1);
+
+// Задание 2
+// let iPhoneReleaseYear = 2017;
+// alert(`Год выпуска первого айфона состоялся в ${iPhoneReleaseYear} году`);
+
+// Задание 3
+// let nameCreation = "Брендан Эйх";
+// alert(`Имя создателя языка JavaScript - ${nameCreation}`);
+
+// Задание 4
+// let a4 = 10;
+// let b4 = 2;
+
+// alert(a4 + b4); // Сумма
+// alert(a4 - b4); // Разность
+// alert(a4 * b4); // Произведение
+// alert(a4 / b4); // Частное
+
+// Задание 5
+// let a5 = 2;
+// let result = a5 ** 5;
+// alert(result);
+
+// Задание 6
+// let a6 = 9;
+// let b6 = 2;
+// alert(a6 % b6);
+
+// Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+// Задание 8
+// let age = prompt("Сколько вам лет?");
+// alert(age);
+
+// Задание 9.0
+// const user = {
+//     name: `Marina`,
+//     age: 26,
+//     asAdmin: true,
+// }
+
+// Задание 9.1
+// user['city of residence'] = 'Barcelona'
+// // console.log(user[`city of residence`]); // Check
+
+// Задание 9.2
+// user.age = 27
+// // console.log(user.age);// Check
+
+// // Задание 9.3
+// delete user[`city of residence`];
+
+// // Задание 9.4
+// let info = prompt(`Какую информацию хотите узнать о пользователе?`, `name`);
+// // console.log(user[info]); // Check
+// alert(user[info]);
+
+// Задание 10
+// let userName = prompt("Как вас зовут?");
+// alert(`Привет, ${userName} !`);
