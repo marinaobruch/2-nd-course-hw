5e82d6c67de4592640821d9c8c10be17d21def18
«Marina» <marina_obruch@mail.ru>
2023-04-01 22:48:24 +0500

    3rd HW JS

---
+++ hw3.js
@@ -0,0 +1,53 @@
+// Задание 1
+// let count = 0;
+
+// while (count < 2) {
+//     console.log("Привет");
+//     count++;
+// }
+
+// Задание 2
+// let i = 1;
+
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+// Задание 3
+// let b = 7;
+
+// while (b <= 22) {
+//     console.log(b);
+//     b++;
+// }
+
+// Задание 4
+// const obj = {
+//     "Коля": "200",
+//     "Вася": "300",
+//     "Петя": "400"
+// };
+
+// for (let salary in obj) {
+//     console.log(`${salary} - зарплата ${obj[salary]} долларов`);
+// }
+
+// Задание 5
+// let n = 1000;
+// let num = 0;
+
+// while (n > 50) {
+//     n = n / 2;
+//     num++;
+// }
+// console.log(n);
+// console.log(num);
+
+// Задание 6
+// for (let dayNumber = 1; dayNumber <= 31; dayNumber++) {
+//     if (dayNumber % 5 == 0) {
+//         console.log(`Сегодня пятница, ${dayNumber} - е число.Необходимо подготовить отчет.`);
+//         continue;
+//     }
+// }
