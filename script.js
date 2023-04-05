027ec59231f856b20de0b5fd2c0082d6c1051628
«Marina» <marina_obruch@mail.ru>
2023-04-05 22:24:47 +0500

    Added HW4(JS tasks), adaptive(CSS task),JS on site

---
+++ script.js
@@ -0,0 +1,16 @@
+// function guessMonth(month) {
+//     if (month === 12 || month === 1 || month === 2) {
+//         console.log(`Зима`);
+//     } else if (month === 3 || month === 4 || month === 5) {
+//         console.log(`Весна`);
+//     } else if (month === 6 || month === 7 || month === 8) {
+//         console.log(`Лето`);
+//     } else if (month === 9 || month === 10 || month === 11) {
+//         console.log(`Осень`);
+//     } else {
+//         console.log(`Вы ввели некорректное значение`);
+//         guessMonth(Number(prompt(`Укажите номер месяца (от 1 до 12)`)));
+//     }
+// }
+
+// guessMonth(Number(prompt(`Укажите номер месяца (от 1 до 12)`)));
