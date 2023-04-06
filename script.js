c92c3ab4c9eff6e05bad8b42effddeec1ac3b78a
«Marina» <marina_obruch@mail.ru>
2023-04-06 21:31:16 +0500

    Added throught "Onclick"

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+// // Запуск кода через addEventListener
+// function guessMonth() {
+//     const month = Number(prompt(`Укажите номер месяца`));
+
+//     if (month === 12 || month === 1 || month === 2) {
+//         console.log(`Зима`);
+//         alert(`Зима`);
+//     } else if (month === 3 || month === 4 || month === 5) {
+//         console.log(`Весна`);
+//         alert(`Весна`);
+//     } else if (month === 6 || month === 7 || month === 8) {
+//         console.log(`Лето`);
+//         alert(`Лето`);
+//     } else if (month === 9 || month === 10 || month === 11) {
+//         console.log(`Осень`);
+//         alert(`Осень`);
+//     } else {
+//         alert(`Вы ввели некорректное значение`);
+//         console.log(`Вы ввели некорректное значение`);
+//     }
+
+// }
+
+// click1.addEventListener("click", guessMonth);
+
+// // Запуск кода через Onclick
 function guessMonth() {
-    let month = Number(prompt(`Укажите номер месяца`));
+    const month = Number(prompt(`Укажите номер месяца`));
 
     if (month === 12 || month === 1 || month === 2) {
         console.log(`Зима`);
@@ -14,9 +40,7 @@ function guessMonth() {
         console.log(`Осень`);
         alert(`Осень`);
     } else {
+        alert(`Вы ввели некорректное значение`);
         console.log(`Вы ввели некорректное значение`);
     }
-
 }
-// guessMonth();
-click1.addEventListener("click", guessMonth);
