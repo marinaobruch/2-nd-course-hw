426c0aba62fc3853612eb63c43818d695897debd
«Marina» <marina_obruch@mail.ru>
2023-04-06 20:57:29 +0500

    Added link on game

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-// function guessMonth(month) {
-//     if (month === 12 || month === 1 || month === 2) {
-//         console.log(`Зима`);
-//     } else if (month === 3 || month === 4 || month === 5) {
-//         console.log(`Весна`);
-//     } else if (month === 6 || month === 7 || month === 8) {
-//         console.log(`Лето`);
-//     } else if (month === 9 || month === 10 || month === 11) {
-//         console.log(`Осень`);
-//     } else {
-//         console.log(`Вы ввели некорректное значение`);
-//         guessMonth(Number(prompt(`Укажите номер месяца (от 1 до 12)`)));
-//     }
-// }
+function guessMonth() {
+    let month = Number(prompt(`Укажите номер месяца`));
 
-// guessMonth(Number(prompt(`Укажите номер месяца (от 1 до 12)`)));
+    if (month === 12 || month === 1 || month === 2) {
+        console.log(`Зима`);
+    } else if (month === 3 || month === 4 || month === 5) {
+        console.log(`Весна`);
+    } else if (month === 6 || month === 7 || month === 8) {
+        console.log(`Лето`);
+    } else if (month === 9 || month === 10 || month === 11) {
+        console.log(`Осень`);
+    } else {
+        console.log(`Вы ввели некорректное значение`);
+    }
+
+    guessMonth();
+}
